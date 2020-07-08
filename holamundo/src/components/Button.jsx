ae935e75a6d3c14edc83b4df0ee53768ba3f67cc
Nicolas Posa <nicoposa57@gmail.com>
2020-07-08 10:06:51 -0300

    state - Events

--- holamundo/src/components/Button.jsx
+++ holamundo/src/components/Button.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
-const Button = props => {
-  const { text } = props
-  return(
-    <div>
-      <button type='Button'>{props.text}</button>
-      <button type='button'>{text}</button>
-    </div>
-  )
+class Button extends React.Component{
+  state = {
+    count: 0
+  }
+  hendleClick = () =>{
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+  render(){
+    const { count } = this.state
+    return(
+      <div>
+        <h1>Manzanas: {count}</h1>
+        <button type="button" onClick={this.hendleClick}>Agregar</button>
+      </div>
+    )
+  }
 }
 
 export default Button
